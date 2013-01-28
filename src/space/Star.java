29d462ca6d7ee5b45b691b553e0db3960da6f877
whaleberg <whaleberg@gmail.com>
2013-01-28 01:53:04 -0500

    Made changes to allow multiple levels of orbits.

--- src/space/Star.java
+++ src/space/Star.java
@@ -14,47 +14,14 @@ import vector.Point2d;
  *
  * @author sailfish
  */
-public class Star implements Orbitable, Drawable{
-    private final PhysicalBody body = new PhysicalBody();
-    private final List<Orbiter> children = new ArrayList<>();
+public class Star extends AbstractOrbitable{
     
     private static final IconType icon = IconType.STAR;
     
     public Star(Point2d position, double mass, double radius){
-        this.body.setPos(position);
-        this.body.setMass(mass);
-        this.body.setRadius(radius);
-    }
-    
-    @Override
-    public Point2d getPos() {
-        return body.getPos();
-    }
-
-    @Override
-    public Collection<Orbiter> getDirectOrbiting() {
-       return Collections.unmodifiableCollection(children); 
-    }
-
-    @Override
-    public double getMass() {
-        return body.getMass();
-    }
-
-    @Override
-    public double getRadius() {
-        return body.getRadius();
-    }
-
-    @Override
-    public boolean addOrbiter(Orbiter orb) {
-        orb.setParent(this);
-        return children.add(orb);
-    }
-
-    @Override
-    public boolean removeOrbiter(Orbiter orb) {
-       return children.remove(orb);
+        this.getBody().setPos(position);
+        this.getBody().setMass(mass);
+        this.getBody().setRadius(radius);
     }
 
     @Override
@@ -62,11 +29,5 @@ public class Star implements Orbitable, Drawable{
         return icon;
     }
 
-    @Override
-    public void age(long seconds) {
-        for(Orbiter orb: children){
-            orb.age(seconds);
-        }
-    }
     
 }
