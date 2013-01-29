fd86b7f1c794364fdace1ec95fdbbbecca73031b
whaleberg <whaleberg@gmail.com>
2013-01-29 01:59:32 -0500

    added simple tests of several starmap methods

---
+++ test/space/StarMapTest.java
@@ -0,0 +1,79 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package space;
+
+import java.util.List;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableSet;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import vector.Point2d;
+
+/**
+ *
+ * @author sailfish
+ */
+public class StarMapTest {
+    
+    private final Drawable d1 = new Star(new Point2d(5,5), 10, 10);
+    private final Drawable d2 = new Star(new Point2d(10,10), 10, 10);
+    private final StarMap map = new StarMap();
+   
+    
+    public StarMapTest() {
+         map.add(d1);
+         map.add(d2);
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+
+
+
+    /**
+     * Test of watch method, of class StarMap.
+     */
+    @Test
+    public void testWatch() {
+        System.out.println("watch");
+        Rect r = new Rect(new Point2d(), 20,20);
+        StarMap instance = map;
+        ObservableSet<Drawable> expResult = FXCollections.observableSet(d1,d2);
+        ObservableSet<Drawable> result = instance.watch(r);
+        assert(expResult.equals(result));
+    }
+
+    /**
+     * Test of getBounds method, of class StarMap.
+     */
+    @Test
+    public void testGetBounds() {
+        System.out.println("getBounds");
+        StarMap instance = new StarMap();
+        instance.add(d1);
+        instance.add(d2);
+        Rect expResult = new Rect(new Point2d(5,5), new Point2d(10,10));
+        Rect result = instance.getBounds();
+        assertEquals(expResult, result);
+   }
+}
