d03fdefcff9fc2bb1fd98b800cae2afe87617628
whaleberg <whaleberg@gmail.com>
2013-01-29 01:24:54 -0500

    Added equals and hashcode to point2d, the lack thereof was causing Rect.equals to not work.

--- src/vector/Point2d.java
+++ src/vector/Point2d.java
@@ -31,7 +31,7 @@
 
 package vector;
 
-import java.beans.VetoableChangeListener;
+import java.util.Objects;
 
 /**
  * A 2 element point that is represented by double precision floating 
@@ -43,6 +43,28 @@ public class Point2d implements java.io.Serializable {
     // Compatible with 1.1
     static final long serialVersionUID = 1133748791492571954L;
     private final Vector2d vect;
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.vect);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Point2d other = (Point2d) obj;
+        if (!Objects.equals(this.vect, other.vect)) {
+            return false;
+        }
+        return true;
+    }
     /**
      * Constructs and initializes a Point2d from the specified xy coordinates.
      * @param x the x coordinate
--- test/space/RectTest.java
+++ test/space/RectTest.java
@@ -4,13 +4,13 @@
  */
 package space;
 
-import javax.vecmath.Point2d;
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import vector.Point2d;
 
 /**
  *
@@ -43,12 +43,39 @@ public class RectTest {
     @Test
     public void testContains() {
         System.out.println("contains");
-        Point2d p = null;
-        Rect instance = null;
-        boolean expResult = false;
+        Point2d p = new Point2d(5,5);
+        Rect instance = new Rect(new Point2d(), 10, 10);
+        boolean expResult = true;
         boolean result = instance.contains(p);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    }
+
+    
+    /**
+     * Test of alternate two point constructor method, of class Rect.
+     */
+    @Test
+    public void twoPointConstructor() {
+        System.out.println("twoPointConstructor");
+        Rect r = new Rect(new Point2d(), new Point2d(5,10));
+        Rect instance = new Rect(new Point2d(5,10), new Point2d());
+        boolean expResult = true;
+        boolean result = instance.equals(r);
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of matchesAspectRatio method, of class Rect.
+     */
+    @Test
+    public void testMatchesAspectRatio() {
+        System.out.println("matchesAspectRatio");
+        Rect r = new Rect(new Point2d(), new Point2d(5,10));
+        Rect instance = new Rect(new Point2d(), new Point2d(10,20));
+        boolean expResult = true;
+        boolean result = instance.matchesAspectRatio(r);
+        assertEquals(expResult, result);
+
     }
 }
